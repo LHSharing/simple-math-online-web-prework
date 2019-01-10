2199a1243731cb86bea25eb14896c8f5c534545c
Lu Daley <daleyluis@gmail.com>
2019-01-10 01:00:48 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,13 +1,13 @@
 require 'pry'
 # 1
 def addition(num1, num2)
-num1 + num2
+  num1 + num2
 end
 
 
 #2
 def subtraction(num1, num2)
-num1 - num2
+  num1 - num2
 end
 
 #3
@@ -17,18 +17,21 @@ end
 
 #4
 def multiplication(num1, num2)
-num1 * num2
+  num1 * num2
 end
 
 #5
 def modulo(num1, num2)
+  
 end
 
 #6
 def square_root(num)
+  
 end
 
 #7
 def order_of_operation(num1, num2, num3, num4)
+  
   #Hint:  __ + (( __ * __ ) / __ )
 end
