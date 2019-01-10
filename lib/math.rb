0f55a039918b1cb6ab37d9ae1bc8e905bf43dbdf
Lu Daley <daleyluis@gmail.com>
2019-01-10 01:17:34 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,21 +1,38 @@
+require 'pry'
+# 1
 def addition(num1, num2)
+  num1 + num2
 end
 
+
+#2
 def subtraction(num1, num2)
+  num1 - num2
 end
 
+#3
 def division(num1, num2)
+  num1 / num2
 end
 
+#4
 def multiplication(num1, num2)
+  num1 * num2
 end
 
+#5
 def modulo(num1, num2)
+  num1 % num2
 end
 
+#6
 def square_root(num)
+  Math.sqrt(num)
 end
 
+#7
 def order_of_operation(num1, num2, num3, num4)
+  num1 + ((num2 * num3) / num4)
+  
   #Hint:  __ + (( __ * __ ) / __ )
 end
